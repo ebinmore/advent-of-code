4768677de49838792e054d13a7dd35a31e01f323
Eric Binmore <eric.binmore@greatersudbury.ca>
2015-12-04 16:13:40 -0500

    Input as array of hashes

---
+++ day02/a.rb
@@ -0,0 +1,10 @@
+include 'input'
+
+# calculate area of all sides
+# calculate area of wrapping paper needed = area of all sides + area of smallest side
+# fn:
+# => top, bottom = width x depth
+# => front, back = width x height
+# => right, left = depth x height
+
+
