ff9a306900736b586f6c3386b4d7811c60d289c7
Eric Binmore <eric.binmore@greatersudbury.ca>
2015-12-08 16:29:27 -0500

    Fixes bug using V instead of v; adds a lot of debugging noise

--- day03/santa.rb
+++ day03/santa.rb
@@ -6,16 +6,23 @@ class Santa
     @current_position = params.fetch(:starting_position, [0,0])
     @directions = params.fetch(:directions, "")
     @locations_delivered_to = Set.new { @current_position }
+    @new_locations = 0
+    @existing_locations = 0
+    @total_deliveries = 0
   end
 
   def deliver_next_present(direction)
     @current_position[0] = @current_position[0] + 1 if direction == "^"
-    @current_position[0] = @current_position[0] - 1 if direction == "V"
+    @current_position[0] = @current_position[0] - 1 if direction == "v"
     @current_position[1] = @current_position[1] + 1 if direction == ">"
     @current_position[1] = @current_position[1] - 1 if direction == "<"
-    @locations_delivered_to.add?(@current_position)
+    puts "#{direction} Delivering to [#{@current_position[0]}, #{@current_position[1]}] \t\t new? #{!@locations_delivered_to.include?(@current_position)}"
+    
+    @new_locations += 1 unless @locations_delivered_to.include?(@current_position)
+    @existing_locations += 1 if @locations_delivered_to.include?(@current_position)
+    @total_deliveries += 1
 
-    puts "Delivering to [#{@current_position[0]}, #{@current_position[1]}]"
+    @locations_delivered_to << @current_position unless @locations_delivered_to.include?(@current_position)
   end
 
   def deliver_all_presents
@@ -24,6 +31,8 @@ class Santa
 
   def deliveries
     @locations_delivered_to
+
+    puts "Total deliveries #{@total_deliveries} \tNew Locations #{@new_locations} \tExisting Locations #{@existing_locations} "
   end
 
 end
