a927dccff2a0b7d41b3b0277fa9b26dfd2f6c20e
Eric Binmore <eric.binmore@greatersudbury.ca>
2015-12-08 16:12:05 -0500

    Switched to Set instead of Array for Santas deliveries

--- day03/a.rb
+++ day03/a.rb
@@ -5,4 +5,4 @@ horrible_directions = IO.read("directions.txt")
 santa = Santa.new(directions: horrible_directions)
 santa.deliver_all_presents
 
-puts "Santa delivered to #{santa.deliveries.uniq.count} locations."
+puts "Santa delivered to #{santa.deliveries.size} locations."
--- day03/santa.rb
+++ day03/santa.rb
@@ -1,9 +1,11 @@
+require 'set'
+
 class Santa
 
   def initialize(params = {})
     @current_position = params.fetch(:starting_position, [0,0])
     @directions = params.fetch(:directions, "")
-    @locations_delivered_to = Array.new { @current_position }
+    @locations_delivered_to = Set.new { @current_position }
   end
 
   def deliver_next_present(direction)
@@ -11,7 +13,7 @@ class Santa
     @current_position[0] = @current_position[0] - 1 if direction == "V"
     @current_position[1] = @current_position[1] + 1 if direction == ">"
     @current_position[1] = @current_position[1] - 1 if direction == "<"
-    @locations_delivered_to << @current_position
+    @locations_delivered_to.add?(@current_position)
 
     puts "Delivering to [#{@current_position[0]}, #{@current_position[1]}]"
   end
