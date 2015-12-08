a5c140683886dcd70839a88ac6c601bef42f284a
Eric Binmore <eric.binmore@greatersudbury.ca>
2015-12-08 08:49:02 -0500

    Showing my rust ruby-foo... require files and changed to module

--- day02/a.rb
+++ day02/a.rb
@@ -1,5 +1,5 @@
-include 'present'
-include 'input'
+require_relative 'present'
+require_relative 'input'
 
 # calculate area of all sides
 # calculate area of wrapping paper needed = area of all sides + area of smallest side
@@ -8,4 +8,4 @@ include 'input'
 # => front, back = width x height
 # => right, left = depth x height
 
-puts "Total wrapping paper required is #{presents.map(&:wrapping_paper_required).reduce(0, &:+)} m2."
+puts "Total wrapping paper required is #{SantasPresents::PRESENTS_TO_WRAP.map(&:wrapping_paper_required).reduce(0, &:+)} m2."
