d3fef4251bec11058c781fdd278ac7724932e527
Eric Binmore <eric@pottedbird.ca>
2015-12-08 21:52:29 -0500

    Fleshed out the doodle

--- day03/b.rb
+++ day03/b.rb
@@ -2,3 +2,13 @@
 # directions, if array, Santa gets all the even numbers, including robo-Santa gets all elements with odd indices
 # both start at zero
 
+require_relative "santa"
+
+horrible_directions = IO.read("directions.txt")
+
+santa = Santa.new(directions: horrible_directions_even_elements)
+robo_santa = Santa.new(directions: horrible_directions_odd_elements)
+
+santa.deliver_all_presents.union(robo_santa.deliver_all_presents)
+
+puts "Santa delivered to #{santa.deliveries.size} locations."
