ad089110d2fe8d177fbb5372e53f08c9f8349951
Eric Binmore <eric.binmore@greatersudbury.ca>
2015-12-09 13:35:38 -0500

    Splits directions, assigns routes to Santa & robo-Santa, and joins the set of locations

--- day03/b.rb
+++ day03/b.rb
@@ -1,14 +1,21 @@
 # Santa moves first, followed by robo-Santa
 # directions, if array, Santa gets all the even numbers, including robo-Santa gets all elements with odd indices
 # both start at zero
-
 require_relative "santa"
 
 horrible_directions = IO.read("directions.txt")
 
-santa = Santa.new(directions: horrible_directions_even_elements)
-robo_santa = Santa.new(directions: horrible_directions_odd_elements)
+santas_directions = horrible_directions.chars.select.with_index { |direction, index| (index + 1) % 2 == 0 }
+robo_santas_directions = horrible_directions.chars.select.with_index { |direction, index| (index + 1) % 2 == 1 }
 
-santa.deliver_all_presents.union(robo_santa.deliver_all_presents)
+santa = Santa.new(directions: santas_directions.join)
+robo_santa = Santa.new(directions: robo_santas_directions.join)
 
-puts "Santa delivered to #{santa.deliveries.size} locations."
+santa.deliver_all_presents
+robo_santa.deliver_all_presents
+puts "Santa's deliveries #{santa.deliveries.size}"
+puts "robo-Santa's deliveries #{robo_santa.deliveries.size}"
+
+all_deliveries = Set.new
+all_deliveries = santa.deliveries | robo_santa.deliveries
+puts "Santa & robo-Santa delivered to #{all_deliveries.size} locations."
