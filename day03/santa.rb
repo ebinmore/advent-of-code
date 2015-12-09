3026d8bb8414abc838131cfc727db2bfaf94b07c
Eric Binmore <eric.binmore@greatersudbury.ca>
2015-12-09 13:33:53 -0500

    Fixes issue with passing array by reference - all entries in set were the same

--- day03/santa.rb
+++ day03/santa.rb
@@ -16,17 +16,18 @@ class Santa
     @current_position[0] = @current_position[0] - 1 if direction == "v"
     @current_position[1] = @current_position[1] + 1 if direction == ">"
     @current_position[1] = @current_position[1] - 1 if direction == "<"
-    puts "#{direction} Delivering to [#{@current_position[0]}, #{@current_position[1]}] \t\t new? #{!@locations_delivered_to.include?(@current_position)}"
     
     @new_locations += 1 unless @locations_delivered_to.include?(@current_position)
     @existing_locations += 1 if @locations_delivered_to.include?(@current_position)
     @total_deliveries += 1
 
-    @locations_delivered_to << @current_position unless @locations_delivered_to.include?(@current_position)
+    @locations_delivered_to << @current_position.clone
   end
 
   def deliver_all_presents
     @directions.each_char { |direction| deliver_next_present(direction) }
+
+    puts "Total deliveries #{@total_deliveries} \tNew #{@new_locations} \tExisting #{@existing_locations}"
   end
 
   def deliveries
