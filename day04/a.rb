3e2d6dcfb2f53cc5d3ae6557ac53554e9259ce98
Eric Binmore <eric.binmore@greatersudbury.ca>
2015-12-11 09:50:18 -0500

    Digest << value then Digest.hexdigest keeps internal state :-(

--- day04/a.rb
+++ day04/a.rb
@@ -1,6 +1,6 @@
 # let's try a brute force method... 
 # start at 1 and count up until with hit an MD5 hash that starts with '00000'
-
+require 'set'
 require 'digest'
 
 md5 = Digest::MD5.new
@@ -9,12 +9,9 @@ md5 = Digest::MD5.new
 i = 1
 secret_key = 'iwrupvqb'
 
-md5 << "#{secret_key}#{i}"
-puts "#{md5.hexdigest}\t#{secret_key}#{i}"
-while (md5.hexdigest[0,5] != '00000') do
+while (md5.hexdigest("#{secret_key}#{i}")[0,5] != '00000') do
   i = i + 1
-  md5 << "#{secret_key}#{i}"
-  puts "#{md5.hexdigest}\t#{secret_key}#{i}\t#{md5.hexdigest[0,5]}"
+  puts "#{md5.hexdigest("#{secret_key}#{i}")}\t#{secret_key}#{i}\t#{md5.hexdigest("#{secret_key}#{i}")[0,5]}"
 end
 
 puts i
