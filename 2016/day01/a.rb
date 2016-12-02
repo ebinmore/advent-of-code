d908cd929be49f02143a9fd6c5c5bdfcf88da02d
Eric Binmore <eric.binmore@voxmedia.com>
2016-12-02 15:16:37 -0500

    Many changes to many issues

--- 2016/day01/a.rb
+++ 2016/day01/a.rb
@@ -5,7 +5,7 @@ easter_bunny_recruiting_document = IO.read("a.input")
 
 taxi = Taxi.new
 easter_bunny_recruiting_document.split(",").map(&:strip).each do |instruction|
-  turn = instruction
+  turn = instruction[0]
   puts "instruction = #{instruction}"
   taxi.turn_left if turn == "L"
   taxi.turn_right if turn == "R"
@@ -15,4 +15,4 @@ easter_bunny_recruiting_document.split(",").map(&:strip).each do |instruction|
 end
 
 puts "The final location is #{taxi.current_position}, facing #{taxi.current_facing.to_s}"
-puts "Total distance away is #{taxi.current_position[0] + taxi.current_position[1]} blocks away."
+puts "Total distance away is #{taxi.current_position[0].abs + taxi.current_position[1].abs} blocks away."
--- 2016/day01/taxi.rb
+++ 2016/day01/taxi.rb
@@ -1,6 +1,6 @@
 class Taxi
   ORIGIN = [0,0]
-  DIRECTION = {north: [1,0], east: [0,1], south: [-1,0], west: [0, -1]}
+  DIRECTIONS = {north: [1,0], east: [0,1], south: [-1,0], west: [0, -1]}
 
   def initialize(params = {starting_location: ORIGIN, starting_direction: :north})
     @current_facing = params[:starting_direction]
@@ -8,17 +8,23 @@ class Taxi
   end
 
   def turn_left
-    @current_facing = DIRECTION.keys[DIRECTIONS.keys.index(@current_facing) - 1]
+    print "currently facing #{@current_facing.to_s}, turning left "
+    @current_facing = DIRECTIONS.keys[DIRECTIONS.keys.index(@current_facing) - 1]
+    puts "to face  #{@current_facing.to_s}"
   end
 
   def turn_right
-    @current_facing = DIRECTION.keys[DIRECTION.keys.index(@current_facing) + 1]
+    print "currently facing #{@current_facing.to_s}, turning right "
+    @current_facing = DIRECTIONS.keys[(DIRECTIONS.keys.index(@current_facing) + 1) % DIRECTIONS.keys.count]
+    puts "to face #{@current_facing.to_s}"
   end
 
   def travel(distance)
-    x = DIRECTION[@current_facing][0] * distance
-    y = DIRECTION[@current_facing][1] * distance
+    x = DIRECTIONS[@current_facing][0] * distance
+    y = DIRECTIONS[@current_facing][1] * distance
+    print "currently at #{@current_position}, travelling [#{x}, #{y}] to "
     @current_position = [@current_position[0] + x, @current_position[1] + y]
+    puts "#{@current_position}"
   end
 
   def current_facing
