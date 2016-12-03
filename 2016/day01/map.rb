79b25de53030840e8ec75597dcfbc25d8e74c5ad
Eric Binmore <eric.binmore@voxmedia.com>
2016-12-02 23:42:00 -0500

    Handles interation over negative ranges
    But stepping back and looking at the process, Im not very confident that the result is going to be correct. I can check because I have adventofcode.com to tell me if I am right or if I am wrong, but that leave me feeling a little hollow.

--- 2016/day01/map.rb
+++ 2016/day01/map.rb
@@ -1,6 +1,22 @@
 
 class Map
 
+  @draw_x = Proc.new do |x|
+    point = [x, @pen[1]].to_s
+    @canvas[point] = 0 unless @canvas.include?(point)
+    @canvas[point] += 1
+    puts "adding point #{point}:#{@canvas[point]}"
+    @intersections << [x, @pen[1]] if @canvas[point] > 1
+  end
+
+  @draw_y = Proc.new do |y|
+    point = [@pen[0], y].to_s
+    @canvas[point] = 0 unless @canvas.include?(point)
+    @canvas[point] += 1
+    puts "adding point #{point}:#{@canvas[point]}"
+    @intersections << [@pen[0], y] if @canvas[point] > 1
+  end
+
   def initialize(params = { start: [0,0] })
     @pen = params[:start]
     @canvas = {}
@@ -11,7 +27,10 @@ class Map
   def trace_route(destination)
     puts "tracing a route from #{@pen} to #{destination}"
     # we drop(1) because (0..0).each iterates once, but our @pen is already there...
-    (@pen[0]..destination[0]).drop(1).each do |x|
+    # next adventure (3..1).each does not iterate from 3 to 1. we can't trace our route west or south :-\
+
+    #  a.downto(b)
+    (@pen[0] > destination[0] ? @pen[0].downto(destination[0]) : @pen[0]..destination[0]).drop(1).each do |x|
       point = [x, @pen[1]].to_s
       @canvas[point] = 0 unless @canvas.include?(point)
       @canvas[point] += 1
@@ -19,7 +38,7 @@ class Map
       @intersections << [x, @pen[1]] if @canvas[point] > 1
     end
 
-    (@pen[1]..destination[1]).drop(1).each do |y|
+    (@pen[1] > destination[1] ? @pen[1].downto(destination[1]) : @pen[1]..destination[1]).drop(1).each do |y|
       point = [@pen[0], y].to_s
       @canvas[point] = 0 unless @canvas.include?(point)
       @canvas[point] += 1
