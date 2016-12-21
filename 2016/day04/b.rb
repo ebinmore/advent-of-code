4210e9498fb2b335f5efb55ec5983e96f9be447e
Eric Binmore <eric.binmore@voxmedia.com>
2016-12-20 22:55:57 -0500

    Fixed and working

--- 2016/day04/b.rb
+++ 2016/day04/b.rb
@@ -4,15 +4,16 @@ possible_rooms = {}
 File.open("a.input").each_line do |line|
   room = Room.new(line.strip)
   if room.valid?
-    puts "Decrypting room #{line.strip}"
+    # puts "Decrypting room #{line.strip}"
     decrypted_room_name  = room.decrypt
-    puts "\t\t#{decrypted_room_name} - #{room.sector_id}"
+    puts "#{decrypted_room_name} - #{room.sector_id}"
 
-    possible_rooms[decrypted_room_name] = room.sector_id if decrypted_room_name.include?("north pole")
+    possible_rooms[decrypted_room_name] = room.sector_id if decrypted_room_name.include?("north")
   end
 end
-
+puts
 puts "Possbile rooms:"
+puts "==============="
 possible_rooms.keys.each do |key|
   puts "#{key} - sector_id: #{possible_rooms[key]}"
 end
--- 2016/day04/room.rb
+++ 2016/day04/room.rb
@@ -2,14 +2,9 @@
 class Room
 
   def initialize(encrypted_name)
-    puts
     @encrypted_name = encrypted_name
-    # puts "encrypted name = #{@encrypted_name}"
-
     @sector_id, @checksum = encrypted_name.split("-").pop.split("[")
     @checksum = @checksum.chop
-    # puts "sector_id = #{@sector_id}"
-    # puts "checksum = #{@checksum}"
   end
 
   def sector_id
@@ -24,47 +19,37 @@ class Room
     # @checksum is the provided
     # checksum is the calculated
     checksum = compute_checksum
-
-    # puts "#{@encrypted_name} decrypted to #{checksum} which #{checksum == @checksum ? 'matches' : 'does not match'} the checksum #{@checksum}."
-
     checksum == @checksum
   end
 
   def compute_checksum
     letters = @encrypted_name.split("-").map(&:chars).flatten
-    # puts "letters = #{letters}"
 
     commonality = letters.group_by { |i| i } # produces { "a": [a,a,a,a], "b": [b,b,b], ... "z": [z]}
     commonality.update(commonality) { |key, value| value.count } # { "a": 4, "b": 3, ..., "z": 1 }
-    # puts "letter frequency = #{commonality}"
+
     # takes { a: 5, b: 2, c: 3, d: 3, e: 3} => {5: [a], 2: [b], 3: [c, d, e]}
     commonality = commonality.reduce({}) do |hash, (key, value)|
       hash[value] = [] unless hash.keys.include? value
       hash[value] << key
       hash
     end
-    # puts "frequency of letters = #{commonality}"
     commonality = commonality.sort.reverse.to_h # sorts by key highest to lowest
-    # puts "ordered highest to lowest frequency = #{commonality}"
     commonality.update(commonality) { |key, value| value.sort } # sorts each value array
     commonality.values.flatten[0..(@checksum.length - 1)].join
   end
 
   def decrypt
-    $stdout.sync
     words = @encrypted_name.split("-")
     words.pop # get rid of the sector_id & checksum
 
     decrypted = []
     alphabet = "abcdefghijklmnopqrstuvwxyz".chars
     words.each do |word|
-      print "translating #{word} to "
       decrypted_word = []
       word.downcase.each_char do |char|
         decrypted_word << alphabet[(alphabet.index(char) + sector_id) % alphabet.count]
-        print decrypted_word.last
       end
-      print "\n"
       decrypted << decrypted_word
     end
 
