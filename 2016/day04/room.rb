fce33ab359dc1ffeff0606595033e30214a76791
Eric Binmore <eric.binmore@voxmedia.com>
2016-12-20 22:42:13 -0500

    Corrected misunderstanding on functionality

--- 2016/day04/room.rb
+++ 2016/day04/room.rb
@@ -4,12 +4,12 @@ class Room
   def initialize(encrypted_name)
     puts
     @encrypted_name = encrypted_name
-    puts "encrypted name = #{@encrypted_name}"
+    # puts "encrypted name = #{@encrypted_name}"
 
     @sector_id, @checksum = encrypted_name.split("-").pop.split("[")
     @checksum = @checksum.chop
-    puts "sector_id = #{@sector_id}"
-    puts "checksum = #{@checksum}"
+    # puts "sector_id = #{@sector_id}"
+    # puts "checksum = #{@checksum}"
   end
 
   def sector_id
@@ -25,27 +25,27 @@ class Room
     # checksum is the calculated
     checksum = compute_checksum
 
-    puts "#{@encrypted_name} decrypted to #{checksum} which #{checksum == @checksum ? 'matches' : 'does not match'} the checksum #{@checksum}."
+    # puts "#{@encrypted_name} decrypted to #{checksum} which #{checksum == @checksum ? 'matches' : 'does not match'} the checksum #{@checksum}."
 
     checksum == @checksum
   end
 
   def compute_checksum
     letters = @encrypted_name.split("-").map(&:chars).flatten
-    puts "letters = #{letters}"
+    # puts "letters = #{letters}"
 
     commonality = letters.group_by { |i| i } # produces { "a": [a,a,a,a], "b": [b,b,b], ... "z": [z]}
     commonality.update(commonality) { |key, value| value.count } # { "a": 4, "b": 3, ..., "z": 1 }
-    puts "letter frequency = #{commonality}"
+    # puts "letter frequency = #{commonality}"
     # takes { a: 5, b: 2, c: 3, d: 3, e: 3} => {5: [a], 2: [b], 3: [c, d, e]}
     commonality = commonality.reduce({}) do |hash, (key, value)|
       hash[value] = [] unless hash.keys.include? value
       hash[value] << key
       hash
     end
-    puts "frequency of letters = #{commonality}"
+    # puts "frequency of letters = #{commonality}"
     commonality = commonality.sort.reverse.to_h # sorts by key highest to lowest
-    puts "ordered highest to lowest frequency = #{commonality}"
+    # puts "ordered highest to lowest frequency = #{commonality}"
     commonality.update(commonality) { |key, value| value.sort } # sorts each value array
     commonality.values.flatten[0..(@checksum.length - 1)].join
   end
@@ -61,8 +61,8 @@ class Room
       print "translating #{word} to "
       decrypted_word = []
       word.downcase.each_char do |char|
-        decrypted_word << alphabet[(alphabet.index(char) + 1) % alphabet.count]
-        print decrypted_word
+        decrypted_word << alphabet[(alphabet.index(char) + sector_id) % alphabet.count]
+        print decrypted_word.last
       end
       print "\n"
       decrypted << decrypted_word
