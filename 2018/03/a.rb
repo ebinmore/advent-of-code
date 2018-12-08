de375f1477a17d2314054f6a6fced5fd96bbc2e6
Eric Binmore <eric.binmore@voxmedia.com>
2018-12-08 11:00:00 -0500

    Move Pattern into a class of its own

--- 2018/03/a.rb
+++ 2018/03/a.rb
@@ -1,20 +1,4 @@
-class Pattern
-  attr_reader :id, :upper_left_corner, :width, :height
-
-  def initialize(data)
-    parse_expression = /#(\d)+\s@\s(\d+),(\d+):\s(\d+)x(\d+)/
-    parsed = parse_expression.match(data)
-    @id = parsed[1]
-    @upper_left_corner = [parsed[2].to_i, parsed[3].to_i]
-    @width = parsed[4].to_i
-    @height = parsed[5].to_i
-  end
-
-  def area
-    return width * height
-  end
-end
-
+require_relative './pattern'
 
 def map_pattern_to_canvas(canvas, pattern)
   leftmost, uppermost = pattern.upper_left_corner
---
+++ 2018/03/pattern.rb
@@ -0,0 +1,16 @@
+class Pattern
+  attr_reader :id, :upper_left_corner, :width, :height
+
+  def initialize(data)
+    parse_expression = /#(\d)+\s@\s(\d+),(\d+):\s(\d+)x(\d+)/
+    parsed = parse_expression.match(data)
+    @id = parsed[1]
+    @upper_left_corner = [parsed[2].to_i, parsed[3].to_i]
+    @width = parsed[4].to_i
+    @height = parsed[5].to_i
+  end
+
+  def area
+    return width * height
+  end
+end
