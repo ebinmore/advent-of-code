67f4eb74c21ffc3d3597bc3211a88a0a74e8896f
Eric Binmore <eric.binmore@voxmedia.com>
2018-12-08 11:09:09 -0500

    Using data

--- 2018/03/a.rb
+++ 2018/03/a.rb
@@ -10,33 +10,48 @@ def map_pattern_to_canvas(canvas, pattern)
   end
 end
 
-test_data = [
-  '#1 @ 1,3: 4x4',
-  '#2 @ 3,1: 4x4',
-  '#3 @ 5,5: 2x2'
-]
-expected_value = 4
+def testing_setup
+  @data = [
+    '#1 @ 1,3: 4x4',
+    '#2 @ 3,1: 4x4',
+    '#3 @ 5,5: 2x2'
+  ]
+  @expected_value = 4
 
-# Three dimensional array 10 x 10 x nil
-canvas = Array.new(10) { Array.new(10) { [] } } # [ [ [], ..., [] ], ..., [ [], ..., [] ] ]
-# visually
-# 0 1 2 . . . y
-# 1
-# 2
-# .
-# .
-# .
-# x
+  # Three dimensional array 10 x 10 x nil
+  @canvas = Array.new(10) { Array.new(10) { [] } } # [ [ [], ..., [] ], ..., [ [], ..., [] ] ]
+  # visually
+  # 0 1 2 . . . y
+  # 1
+  # 2
+  # .
+  # .
+  # .
+  # x
+end
+
+def setup
+  @data = IO.readlines('./data').map(&:strip)
+  size = 1000
+  @canvas = Array.new(size) { Array.new(size) { [] } }
+end
+
+
+if ENV['TESTING']
+  testing_setup
+else
+  setup
+end
 
 # map pattern data to the canvas
-test_data.each do |data|
+@data.each do |data|
   pattern = Pattern.new(data)
-  map_pattern_to_canvas(canvas, pattern)
+  map_pattern_to_canvas(@canvas, pattern)
 end
 
 # find overlaps
 square_inches_of_overlap = 0
-canvas.each do |x|
+@canvas.each do |x|
   x.each do |y|
     square_inches_of_overlap +=1 if y.count > 1
   end
