e562d4104e9ed20d7479c9189c80714f1ce5d223
Eric Binmore <eric.binmore@voxmedia.com>
2019-02-09 09:38:32 -0500

    Day 05 Part 2 working with test data

--- 2018/05/a.rb
+++ 2018/05/a.rb
@@ -1,6 +1,5 @@
 require_relative 'setup'
 require_relative 'reactor'
-require 'byebug'
 
 # puts "Using the test data -- #{Setup.test_data}"
 # final_polymer = Reactor.ignite(Setup.test_data)
@@ -11,5 +10,4 @@ puts "Putting the polymer in the reactor..."
 puts
 final_polymer = Reactor.ignite(Setup.data)
 puts
-byebug
 puts "There are #{final_polymer.length} units left."
---
+++ 2018/05/b.rb
@@ -0,0 +1,31 @@
+require_relative 'setup'
+require_relative 'reactor'
+
+puts "Using the test data -- #{Setup.test_data}"
+polymer = Setup.test_data
+
+
+puts "Removing reagent and reacting"
+reagents = Reactor.breakdown(polymer)
+reacted_polymers = reagents.map do |reagent|
+  stripped = Reactor.scrub(polymer, reagent)
+  puts "stripped #{stripped}"
+  {reagent: reagent, polymer: Reactor.ignite(stripped)}
+end
+
+reacted_polymers.each do |reacted|
+  puts "#{reacted[:reagent]} reduced to #{reacted[:polymer].length}"
+end
+
+shortest_polymer = reacted_polymers.min_by do |reacted|
+  puts "#{reacted[:polymer].length} : #{reacted}"
+  reacted[:polymer].length
+end
+puts "The shortest polymer is #{shortest_polymer} and has a length of #{shortest_polymer[:polymer].length}"
+
+
+# puts "Putting the polymer in the reactor..."
+# puts
+# final_polymer = Reactor.ignite(Setup.data)
+# puts
+# puts "There are #{final_polymer.length} units left."
